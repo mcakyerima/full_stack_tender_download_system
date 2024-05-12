ece056ded799edad009a0ac4f09ae3941246d59d
mcakyerima <mcakyerima@gmail.com>
2024-05-12 01:10:08 +0100

    a

--- app/dashboard/file/page.tsx
+++ app/dashboard/file/page.tsx
@@ -5,7 +5,8 @@ import FilesBrowser from "@/app/dashboard/_components/file-browser";
 export default function FilesPage() {
   return (
     <div>
-      <FilesBrowser title="Your Files" />
+      {/* <FilesBrowser title="Your Files" /> */}
+      Hello world
     </div>
   );
 }
